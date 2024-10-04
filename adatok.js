3ded211e772cad1f0291b5a749afabf2230a0c6e
zltnszekeres <zltn.szekeres@yahoo.com>
2024-10-04 16:00:01 +0200

    adatok felvitele

---
+++ adatok.js
@@ -0,0 +1,30 @@
+export const TERMEKLISTA = [
+{   nev : "Csepel woodlands pro",
+    leiras: "versenyzesre optimalizalt",
+    ar: "226000"
+},
+
+ {  nev : "GT Agressor",
+    leiras: "azoknak, akik a leglobb teljesitmenyt es legsokoldalubb felhasznalast keeresik",
+    ar: "255000"
+},
+{   nev : "Kellys gate 70",
+    leiras: "vadonatuj vaz, kenyelmes, de sportos geometriaval",
+    ar: "670000"
+},
+
+
+{   nev : "Btwin rockrider race 700",
+    leiras: "Az első teljesítményorientált XC MTB alacsony áron",
+    ar: "400000"
+},
+
+
+
+
+
+
+
+
+
+]
